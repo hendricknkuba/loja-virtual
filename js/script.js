c43d81d0b10900c464f678ec2262f0bc8ac898b9
leehendrick <imhendrick16@gmail.com>
2025-03-27 00:19:56 -0400

    feat: create product constructor

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Constructors
+function Product(id, name, price, quantity, img)
+{
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+    this.img = img;
+}
+
+
+
